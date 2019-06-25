17dc7f670925b4ec5582340c271e75f83279a6ff
Caio Rosa <caio.cjcr@gmail.com>
2019-06-25 18:04:19 -0300

    Comment some helper functions with their source

--- frontend/src/utils/helpers.js
+++ frontend/src/utils/helpers.js
@@ -1,15 +1,18 @@
+// Function source: https://github.com/udacity/reactnd-chirper-app
 export function formatDate (timestamp) {
     const d = new Date(timestamp)
     const time = d.toLocaleTimeString('en-US')
     return time.substr(0, 5) + time.slice(-2) + ' | ' + d.toLocaleDateString()
   }
 
+// Function source: https://medium.com/dailyjs/rewriting-javascript-converting-an-array-of-objects-to-an-object-ec579cafbfc7
 export const arrayToObject = (array, keyField) =>
 array.reduce((obj, item) => {
   obj[item[keyField]] = item
   return obj
 }, {})
 
+// Function source: https://github.com/udacity/reactnd-chirper-app
 export function generateUID () {
   return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)
 }
